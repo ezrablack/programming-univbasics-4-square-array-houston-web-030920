1abc19754eaf41647a1eff8208297c51874d1b02
Timothy Morales <timothydanielmorales@gmail.com>
2020-02-01 20:34:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
   new_array =[]
   i = 0
   while array[i] do
-    new_array[i] = i ** 2
+    new_array[i] = array[i] ** 2
     i += 1 
-    p new_array
   end
+  p new_array
 end
